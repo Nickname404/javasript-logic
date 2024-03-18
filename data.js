7a41f171387d5ca9c0ff218bcb42b8cb5a810d98
Nickname404 <userofbloods@gmail.com>
2024-03-18 13:43:22 +0700

    commmit

---
+++ data.js
@@ -0,0 +1,11 @@
+function saklar(parameter){
+let nyala = document.getElementById("nyala1")
+    if(parameter =="on"){
+    nyala1.src = "aset/images/on.gif"
+    }
+    if(parameter =="off"){
+    nyala1.src = "aset/images/off.gif"
+    }  
+    
+    return nyala1
+}
